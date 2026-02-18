633b66879cd806903dfe8005b62b0f6ede995e76
6261vishnumho <7225vishnumahor@gmail.com>
2026-02-18 14:18:15 +0530

    add files

---
+++ conditional_statements.js
@@ -0,0 +1,119 @@
+// let a = prompt("Enter the first number")
+// let b = prompt("Enter the first number")
+
+
+// a = Number(a)
+// b = Number(b)
+
+// alert("sum= " + (a + b))
+
+
+// let a = prompt("Enter the first number")
+// // let b = prompt("Enter the first number")
+
+
+// a = Number(a)
+// // b = Number(b)
+
+
+// if(a>0){
+//     alert("Number is positive = " + a)
+// }
+// else{
+//     alert("Number is negative = "+ a)
+
+// }
+
+
+
+// let a = prompt("Enter the first number")
+// let b = prompt("Enter the secound number")
+// let c = prompt("Enter the third number")
+
+
+
+// a = Number(a)
+// b = Number(b)
+// c = Number(c)
+
+// if(a>b && a>c){
+//        alert("Number is maximum A = "+ a)
+
+// }
+// else if(b>a && b>c){
+//     alert("Number is maxiimum  B = " + b)
+
+// }
+// else if (c > a && c > b) {
+//     alert("Number is maximum  C = " + c)
+
+// }
+
+
+
+// if (a < b && a < c) {
+//     alert("Number is minimum A = " + a)
+
+// }
+// else if (b < a && b < c) {
+//     alert("Number is minimum  B = " + b)
+
+// }
+// else if (c < a && c < b) {
+//     alert("Number is minimum  C = " + c)
+
+// }
+
+
+
+
+
+
+// let a = prompt("Enter the first number")
+
+
+// a = Number(a)
+
+// if(a=="munday"){
+//         alert("DAY 1 = " +a)
+
+// }
+//  else if (a == "tuesday") {
+//     alert("DAY 2 = " + a)
+
+// }
+// else if (a == "wednesday") {
+//     alert("DAY 3 = " + a)
+
+// }
+// else if (a == "thursday") {
+//     alert("DAY 4  = " + a)
+
+// }
+// else if (a == "friday") {
+//     alert("DAY 5  = " + a)
+
+// }
+// else if (a == "saturday") {
+//     alert("DAY 6  = " + a)
+
+// }
+// else if (a == "sunday") {
+//     alert("DAY 7  = " + a)
+
+// }
+
+
+
+let a = prompt("Enter the first number")
+
+a=Number(a)
+
+if(a%4==0 &&a%100 !=0 || a%400==0){
+        alert("This is a leap year = " + a)
+
+}
+else{
+        alert("not a leap year  = " + a)
+
+}
